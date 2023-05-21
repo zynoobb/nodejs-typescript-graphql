e95d841040eb3e339eed6c39d032a9873b369d1b
zynoobb <117588482+zynoobb@users.noreply.github.com>
2023-05-21 13:36:57 +0900

    Test : userService test (미완성)

--- src/resolvers/services/test/user.spec.ts
+++ src/resolvers/services/test/user.spec.ts
@@ -1,12 +1,32 @@
-import { beforeEach, describe, it } from "@jest/globals";
-import { UserService } from "../user";
-import { prismaMock } from "../../../common/prismaJest";
+// import { beforeEach, describe, it } from "@jest/globals";
+// import { UserService } from "../user";
+// import { prismaMock } from "../../../common/prismaJest";
 
-describe("userService", () => {
-  let userService: UserService;
+// describe("userService", () => {
+//   let userService: UserService;
 
-  it("test", () => {
-    const a = 1;
-    expect(a).toBe(1);
-  });
-});
+//   beforeEach(() => {
+//     userService = new UserService();
+//   });
+
+//   describe("findOneByName", () => {
+//     it("should find a user by name", async () => {
+//       const mockedUser = {
+//         id: "123",
+//         email: "test@example.com",
+//         password: "test",
+//         name: "name",
+//         createAt: new Date(),
+//       };
+//       prismaMock.user.findFirst.mockResolvedValueOnce(mockedUser);
+
+//       const email = "test@example.com";
+//       const result = await userService.findOneByName(email);
+
+//       expect(prismaMock.user.findFirst).toHaveBeenCalledWith({
+//         where: { email },
+//       });
+//       expect(result).toEqual(mockedUser);
+//     });
+//   });
+// });
